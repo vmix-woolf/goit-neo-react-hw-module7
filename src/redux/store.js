af84cf65fa98a65aa19eebf1ec4db5f21db0098e
Viacheslav Mykhailov <viacheslav.leo.mykhailov@gmail.com>
2025-08-30 17:46:47 +0300

    step-02, simplify reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,37 +2,11 @@ import { configureStore, combineReducers } from '@reduxjs/toolkit'
 import { contactsReducer } from './contactsSlice'
 import { filtersReducer } from './filtersSlice'
 
-import {
-    persistStore,
-    persistReducer,
-    FLUSH,
-    REHYDRATE,
-    PAUSE,
-    PERSIST,
-    PURGE,
-    REGISTER,
-} from 'redux-persist'
-import storage from 'redux-persist/lib/storage'
-
-const contactsPersistConfig = {
-    key: 'contacts',
-    storage,
-    whitelist: ['items'],
-}
-
 const rootReducer = combineReducers({
-    contacts: persistReducer(contactsPersistConfig, contactsReducer),
+    contacts: contactsReducer,
     filters: filtersReducer,
 })
 
 export const store = configureStore({
     reducer: rootReducer,
-    middleware: getDefaultMiddleware =>
-        getDefaultMiddleware({
-            serializableCheck: {
-                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-            },
-        }),
 })
-
-export const persistor = persistStore(store)
